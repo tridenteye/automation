50f7f70b55fa2380502aaace3a23ad9ae16f371b
Bhavya Bapna <bhabapna@in.ibm.com>
2024-01-30 14:55:36 +0530

    find and replace

--- shell/lib/base.sh
+++ shell/lib/base.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 #set -eu
 
+## Find and replace
+export OLD_STRING="latest-rhnode16"
+export NEW_STRING="latest-rhnode18"
+
 find-replace() {
     echo " --- replacing string --- "
     #grep -rli ${OLD_STRING} * | xargs -I@ sed -i '' "s|${OLD_STRING}|${NEW_STRING}|g" @ # works on mac
